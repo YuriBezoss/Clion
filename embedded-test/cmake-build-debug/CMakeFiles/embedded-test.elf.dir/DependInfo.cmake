0aef7498e4ed4981ec2a9862667008c6a4a7ad67
home office <caokhoa.le@roundsolutions.com>
2021-04-04 00:15:33 +0200

    C init

---
+++ embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/DependInfo.cmake
@@ -0,0 +1,77 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/Projects/Clion/embedded-test/Startup/startup_stm32l476rgtx.s" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Startup/startup_stm32l476rgtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32L476xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32L4xx_HAL_Driver/Inc"
+  "../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32L4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj"
+  "D:/Projects/Clion/embedded-test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj"
+  "D:/Projects/Clion/embedded-test/Src/main.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Src/main.c.obj"
+  "D:/Projects/Clion/embedded-test/Src/stm32l4xx_hal_msp.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Src/stm32l4xx_hal_msp.c.obj"
+  "D:/Projects/Clion/embedded-test/Src/stm32l4xx_it.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Src/stm32l4xx_it.c.obj"
+  "D:/Projects/Clion/embedded-test/Src/syscalls.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Src/syscalls.c.obj"
+  "D:/Projects/Clion/embedded-test/Src/sysmem.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Src/sysmem.c.obj"
+  "D:/Projects/Clion/embedded-test/Src/system_stm32l4xx.c" "D:/Projects/Clion/embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/Src/system_stm32l4xx.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32L476xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32L4xx_HAL_Driver/Inc"
+  "../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32L4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ embedded-test/cmake-build-debug/CMakeFiles/embedded-test.elf.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Src/main.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Src/stm32l4xx_hal_msp.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Src/stm32l4xx_it.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Src/sysmem.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Src/system_stm32l4xx.c.obj"
+  "CMakeFiles/embedded-test.elf.dir/Startup/startup_stm32l476rgtx.s.obj"
+  "embedded-test.elf"
+  "embedded-test.elf.manifest"
+  "embedded-test.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/embedded-test.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
